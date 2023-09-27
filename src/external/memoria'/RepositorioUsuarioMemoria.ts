5b6eb8268825986127a3c8530c44e77d71652382
juliajeske <julia.jeske@wehandle.com.br>
2023-09-26 21:00:45 -0300

    change type of id

--- src/external/memoria'/RepositorioUsuarioMemoria.ts
+++ src/external/memoria'/RepositorioUsuarioMemoria.ts
@@ -1,5 +1,6 @@
 import Usuario from "../../core/usuario/model/Usuario";
 import RepositorioUsuario from "../../core/usuario/service/RepositorioUsuario";
+const uuidv4 = require("uuid/v4")
 
 export default class RepositorioUsuarioMemoria implements RepositorioUsuario {
   private readonly usuarios: Usuario[] = [];
@@ -8,7 +9,7 @@ export default class RepositorioUsuarioMemoria implements RepositorioUsuario {
     return this.usuarios.find((usuario) => usuario.email === email) ?? null;
   }
   async criar(usuario: Usuario): Promise<Usuario> {
-    const novoUsuario = { ...usuario, id: Math.random() };
+    const novoUsuario = { ...usuario, id: uuidv4()}; //Date.now()
     this.usuarios.push(novoUsuario);
 
     return novoUsuario;
